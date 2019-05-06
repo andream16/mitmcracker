e53e993de3717c3864ac4102ff8660882c356158
andreacurve <andrea.medda@imaginecurve.com>
2019-05-06 09:34:28 +0200

    Adding new field to input.

--- input.go
+++ input.go
@@ -15,6 +15,11 @@ var (
 		28: {},
 		32: {},
 	}
+
+	storages = map[string]struct{}{
+		"memory": {},
+		"disk":   {},
+	}
 )
 
 // Input represents the input that can be passed to the command.
@@ -29,6 +34,12 @@ type Input struct {
 	//  - 28-bit
 	//  - 32-bit
 	KeyLength uint
+	// Storage is the type of storage to be used.
+	// Can only be:
+	// - memory
+	// - disk
+	// If no option is provided, memory will be used by default.
+	Storage string
 }
 
 // New returns a new Input.
@@ -36,9 +47,10 @@ func New() (*Input, error) {
 
 	in := &Input{}
 
-	flag.UintVar(&in.KeyLength, "k", 0, "key lenght")
-	flag.StringVar(&in.PlainText, "p", "", "known plain text")
-	flag.StringVar(&in.EncText, "e", "", "known encoded text")
+	flag.UintVar(&in.KeyLength, "key", 0, "key lenght")
+	flag.StringVar(&in.PlainText, "plain", "", "known plain text")
+	flag.StringVar(&in.EncText, "encoded", "", "known encoded text")
+	flag.StringVar(&in.Storage, "storage", "", "storage to be used")
 
 	flag.Parse()
 
@@ -48,7 +60,7 @@ func New() (*Input, error) {
 
 // String returns the string representation of an input.
 func (i *Input) String() string {
-	return fmt.Sprintf("key: %d, plain-text: %s, encoded-text: %s", i.KeyLength, i.PlainText, i.EncText)
+	return fmt.Sprintf("key: %d, plain-text: %s, encoded-text: %s, storage: %s", i.KeyLength, i.PlainText, i.EncText, i.Storage)
 }
 
 func (i *Input) validate() error {
@@ -60,7 +72,13 @@ func (i *Input) validate() error {
 		return errors.Wrap(errInvalidField, "known encrypted text")
 	}
 	if i.PlainText == "" {
-		return errors.Wrap(errInvalidField, "known encrypted text")
+		return errors.Wrap(errInvalidField, "known plain text")
+	}
+	if i.Storage != "" {
+		_, ok := storages[i.Storage]
+		if !ok {
+			return errors.Wrap(errInvalidField, "storage")
+		}
 	}
 	return nil
 }
--- input_test.go
+++ input_test.go
@@ -15,9 +15,18 @@ func TestInput_validate(t *testing.T) {
 			t.Fatalf("expected %s, got %v", errInvalidField, err)
 		}
 	})
-	t.Run("should fail because known encrypted text is empty", func(t *testing.T) {
+	t.Run("should fail because key length is invalig", func(t *testing.T) {
 		i := &Input{
 			KeyLength: 10,
+		}
+		err := i.validate()
+		if errInvalidField != errors.Cause(err) {
+			t.Fatalf("expected %s, got %v", errInvalidField, err)
+		}
+	})
+	t.Run("should fail because known encrypted text is empty", func(t *testing.T) {
+		i := &Input{
+			KeyLength: 24,
 		}
 		err := i.validate()
 		if errInvalidField != errors.Cause(err) {
@@ -28,6 +37,18 @@ func TestInput_validate(t *testing.T) {
 		i := &Input{
 			KeyLength: 28,
 			EncText:   "someEncryptedText",
+		}
+		err := i.validate()
+		if errInvalidField != errors.Cause(err) {
+			t.Fatalf("expected %s, got %v", errInvalidField, err)
+		}
+	})
+	t.Run("should fail because storage is not supported", func(t *testing.T) {
+		i := &Input{
+			KeyLength: 28,
+			EncText:   "someEncryptedText",
+			PlainText: "somePlainText",
+			Storage:   "xyz",
 		}
 		err := i.validate()
 		if errInvalidField != errors.Cause(err) {
